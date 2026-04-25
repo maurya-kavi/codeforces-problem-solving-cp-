b0617d0cab3bb50118c34237bb82814c3e90e8e9
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-25 23:38:45 +0530

    Add A Number Between Two Others [A] from Codeforces

---
+++ 2225/A - A Number Between Two Others/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// macros
+#define ll long long
+#define yes cout << "YES
+"
+#define no cout << "NO
+"
+#define f(i, size) for (int i = 0; i < size; i++)
+// #define ia(a, n) \
+//     int a[n];     \
+//     f(i, n) cin >> a[i]
+ 
+#define iv(v, n)     \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+ 
+// #define M (1000000007)
+#define INF 1000000000000000000LL
+#define mp make_pair
+#define mt make_tuple
+#define nline '
+'
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<pair<int, int>>
+ 
+void solve()
+{
+    ll x,y; cin>>x>>y;
+    if(y==2*x) no;
+    else yes;
+}
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
