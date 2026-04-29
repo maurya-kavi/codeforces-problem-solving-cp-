5277dcf6ae63174ce73e9e1f3e7154e77106360f
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-29 23:55:38 +0530

    Add Red-Black Pairs [C] from Codeforces

---
+++ 2225/C - Red-Black Pairs/solution.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// macros
+#define ll long long
+#define yes cout << "YES
+"
+#define no cout << "NO
+"
+#define f(i, size) for (int i = 0; i < size; i++)
+// #define ia(a, n) \
+//     int a[n];     \
+//     f(i, n) cin >> a[i]
+ 
+#define iv(v, n)     \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+ 
+// #define M (1000000007)
+#define INF 1000000000000000000LL
+#define mp make_pair
+#define mt make_tuple 
+#define nline '
+'
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<pair<int, int>>
+ 
+void solve() {
+    ll n; cin>>n;
+    string s,t; cin>>s>>t;
+    ll res=0;
+    f(i,n){
+        if(s[i]!=t[i]) res++;
+    }
+    f(i,n-1){
+        if(s[i]!=t[i]){
+            if((s[i]==s[i+1]) && (t[i]==t[i+1])){
+                res-=2;
+                i++;
+            }
+        }
+    }
+    cout<<res<<nline;
+}
+ 
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    ll t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
