3848b0fce2298ed96f2c63e3892bbebd0c821d47
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-10 20:14:18 +0530

    Add Construct an Array [A] from Codeforces

---
+++ 2231/A - Construct an Array/solution.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// macros
+#define ll long long
+#define yes cout << "YES
+"
+#define no cout << "NO
+"
+#define f(i, size) for (int i = 0; i < size; i++)
+#define iv(v, n)     \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+ 
+#define INF 1000000000000000000LL
+#define mp make_pair
+#define mt make_tuple
+#define nline '
+'
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<pair<int, int>>
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    for(int i=2*n; i>n; i--){
+        cout<<i<<" ";
+    }
+    cout<<nline;
+}
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
