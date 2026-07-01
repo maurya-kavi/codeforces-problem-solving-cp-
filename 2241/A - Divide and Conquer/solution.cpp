c937eb6f860496fcdc4f882f82c34d25b1b43870
Kavindra Kumar <kavindramehta60@gmail.com>
2026-07-01 16:21:32 +0530

    Add Divide and Conquer [A] from Codeforces

---
+++ 2241/A - Divide and Conquer/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// macros
+#define ll long long
+#define yes cout << "YES
+"
+#define no cout << "NO
+"
+#define f(i, size) for (int i = 0; i < size; i++)
+#define iv(v, n)     \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+ 
+#define INF 1000000000000000000LL
+#define mp make_pair
+#define mt make_tuple
+#define nline '
+'
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<pair<int, int>>
+const int MAX_INF = 1e9;
+const int MIN_INF = -1e9;
+ 
+void solve()
+{
+    ll x, y; cin>>x>>y;
+    (x%y==0? yes : no);
+}
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
